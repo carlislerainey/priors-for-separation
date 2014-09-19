335c7f0c6f41af48e9f4ab44ec0d8e8d224ff58d
carlislerainey <carlislerainey@gmail.com>
2014-09-19 08:17:40 -0400

    add code to perform partial prior predictive checks for BM

--- bm-replication/R/prior-predictive.R
+++ bm-replication/R/prior-predictive.R
@@ -12,7 +12,7 @@ library(metRology)
 # set seed
 # set.seed(8742570)
 
-d <- read.csv("bm-replication/data/bm-small.csv")
+d <- read.csv("bm-replication/data/bm.csv")
 d <- d[, c("warl2", "onenukedyad", "twonukedyad", "logCapabilityRatio", "Ally",
            "SmlDemocracy", "SmlDependence", "logDistance", "Contiguity",
            "MajorPower", "NIGOs")]
@@ -26,5 +26,85 @@ f <- warl2 ~ onenukedyad + twonukedyad + logCapabilityRatio +
 # read in pppd()
 source("R/fn-pppd.R")
 
+# my prior
 ps <- rnorm(10000, 0, 4.5)
-pppd.out <- pppd(f = f, d = d, prior.sims = ps, s = "twonukedyad", s.at = 0, s.at.lo = FALSE)
+my.p <- pppd(f = f, d = d, prior.sims = ps, s = "twonukedyad", s.at = 0, s.at.lo = FALSE)
+
+# skeptical prior
+ps <- rnorm(10000, 0, 2)
+skep.p <- pppd(f = f, d = d, prior.sims = ps, s = "twonukedyad", s.at = 0, s.at.lo = FALSE)
+
+# enthusiastic prior
+ps <- rnorm(10000, 0, 8)
+enth.p <- pppd(f = f, d = d, prior.sims = ps, s = "twonukedyad", s.at = 0, s.at.lo = FALSE)
+
+# gelman's prior
+ps <- rcauchy(10000, 0, 2.5)
+gel.p <- pppd(f = f, d = d, prior.sims = ps, s = "twonukedyad", s.at = 0, s.at.lo = FALSE)
+
+x.max <- 100000
+trunc.my.rr <- my.p$rr[my.p$rr < x.max]
+trunc.gel.rr <- gel.p$rr[gel.p$rr < x.max]
+trunc.skep.rr <- skep.p$rr[skep.p$rr < x.max]
+trunc.enth.rr <- enth.p$rr[enth.p$rr < x.max]
+my.p.trunc <- sum(my.p$rr > x.max)/length(my.p$rr)
+gel.p.trunc <- sum(gel.p$rr > x.max)/length(gel.p$rr)
+skep.p.trunc <- sum(skep.p$rr > x.max)/length(skep.p$rr)
+enth.p.trunc <- sum(enth.p$rr > x.max)/length(enth.p$rr)
+
+
+breaks <- seq(log(1), log(x.max), length.out = 20)
+h1 <- hist(log(trunc.my.rr), breaks = breaks)
+h2 <- hist(log(trunc.gel.rr), breaks = breaks)
+h3 <- hist(log(trunc.skep.rr), breaks = breaks)
+h4 <- hist(log(trunc.enth.rr), breaks = breaks)
+
+add.arrow <- function(p) {
+  x0 <- .88*log(x.max)
+  x1 <- .98*log(x.max)
+  ht <- 200
+  arrows(x0 = x0, y0 = ht, x1 = x1, length = .1)
+  text <- paste(round(100*p), "% of\nsimulations", sep = "")
+  if (round(100*p) == 0) {
+    text <- "< 1% of\nsimulations"
+  }
+  text((x0 + x1)/2, ht, text, pos = 3, cex = .7)
+}
+
+library(compactr)
+xlim0 <- log(c(1, 100000))
+ylim0 <- c(0, max(c(h1$counts, h2$counts, h3$counts, h4$counts)))
+par(mfrow = c(2,2), mar = c(.75,.75,.75,.75), oma = c(2,3,1,1))
+eplot(xlim = xlim0, ylim = ylim0,
+      xlab = "Risk-Ratio (Log Scale)",
+      ylab = "Counts",
+      ylabpos = 2.3,
+      xat = log(c(1, 10, 100, 1000, 10000, 100000)),
+      xticklab = c(1, 10, 100, 1000, 10000, 100000),
+      main = "Informative Normal(0, 4.5) Prior")
+plot(h1, add = TRUE, col = "grey50", border = NA)
+add.arrow(my.p.trunc)
+
+aplot("Gelman's Default Cauchy(0, 2.5) Prior")
+plot(h2, add = TRUE, col = "grey50", border = NA)
+add.arrow(gel.p.trunc)
+
+aplot("Skeptical Normal(0, 2) Prior")
+plot(h3, add = TRUE, col = "grey50", border = NA)
+add.arrow(skep.p.trunc)
+
+aplot("Enthusiastic Normal(0, 8) Prior")
+plot(h4, add = TRUE, col = "grey50", border = NA)
+add.arrow(enth.p.trunc)
+
+
+Q <- round(rbind(my.p$Q[, "risk-ratio"],
+      gel.p$Q[, "risk-ratio"],
+      skep.p$Q[, "risk-ratio"],
+      enth.p$Q[, "risk-ratio"]), 1)
+rownames(Q) <- c("Informative Normal(0, 4.5) Prior",
+                 "Gelman's Default Cauchy(0, 2.5) Prior",
+                 "Skeptical Normal(0, 2) Prior",
+                 "Enthusiastic Normal(0, 8) Prior")
+library(xtable)
+xtable(Q, align = rep("c", ncol(Q) + 1))
