6ef589268d302f2d62b3b9a51c4da2f722d16fee
carlislerainey <carlislerainey@gmail.com>
2014-09-20 12:24:29 -0400

    update mcmc-normal to only place prior on separated variable and run in parallel

--- R/fn-mcmc-normal.R
+++ R/fn-mcmc-normal.R
@@ -1,42 +1,5 @@
-lp.normal <- function(beta, scale.coef, scale.int, X, y) {
-  p <- plogis(matrix(X%*%beta))
-  loglik <- sum(y*log(p)) + sum((1-y)*log(1 - p)) + 
-    log(dcauchy(beta[1], scale = scale.int)) + 
-    sum(log(dnorm(beta[2:length(beta)], sd = scale.coef)))
-  return(loglik)
-}
-
-normal <- function(f, data, scale.coef = 2.5,
-                     scale.int = 10, 
-                   burnin = 500, mcmc = 1000, thin = 1,
-                   tune = 1, verbose = 100) {
-  require(MCMCpack)
-  require(Matrix)
-  require(arm)
-  mf <- model.frame(f, data) 
-  y <- model.response(mf)
-  X <- model.matrix(f, data)
-  X <- Matrix(X)
-  init <- rnorm(ncol(X), 0, 1)
-  cat("Computing proposal distribution...\n")
-  mle <- bayesglm(f, data = d, family = binomial,
-                  prior.scale.for.intercept = scale.int,
-                  prior.scale = scale.coef,
-                  prior.df.for.intercept = df.int,
-                  prior.df = Inf)
-  V <- vcov(mle)
-  print(summary(mle))
-  seed <- round(runif(1, 0, 100000))
-  mcmc <- MCMCmetrop1R(fun = lp.normal, theta.init = init, 
-                       scale.int = scale.int,
-                       scale.coef = scale.coef,
-                       X = X, y = y, burnin = burnin,
-                       mcmc = mcmc, thin = thin, V = V,
-                       tune = tune, seed = seed, 
-                       verbose = verbose)
-  return(mcmc)
-}
-
+# rm(list = ls())
+# 
 # # test
 # set.seed(1234)
 # n <- 100
@@ -45,6 +8,88 @@ normal <- function(f, data, scale.coef = 2.5,
 # beta <- c(-1, 1)
 # y <- rbinom(n, 1, plogis(X%*%beta))
 # d <- data.frame(x1, y)
-# m1 <- normal(y ~ x1, d, mcmc = 100, verbose = 10)
-# plot(m1)
+# f <- y ~ x1
+# data <- d
+# n.chains <- 3
+# sep.var <- "x1"
+# sd <- 4
+
+
+lp.normal <- function(beta, which.var.sep, sd, X, y) {
+  p <- plogis(matrix(X%*%beta))
+  loglik <- sum(y*log(p)) + sum((1-y)*log(1 - p))
+  logprior <- log(dnorm(beta[which.var.sep], mean = 0, sd = sd))
+  logpost <- loglik + logprior
+  return(logpost)
+}
+
+normal <- function(f, data, sep.var, sd = 4.5,
+                   n.sims = 1000, n.burnin = 500,
+                   n.thin = 1, n.chains = 3,
+                   tune = 1, verbose = 100) {
+  require(MCMCpack)
+  require(Matrix)
+  require(arm)
+  require(parallel)
+  mf <- model.frame(f, data) 
+  y <- model.response(mf)
+  X <- model.matrix(f, data)
+  X <- Matrix(X)
+  if (sum(colnames(X) == sep.var) < 1) {
+   stop(paste(sep.var,  " is not a variable in the model", sep = ""))
+  }
+  which.var.sep <- which(colnames(X) == sep.var)
+  init <- rnorm(ncol(X), 0, 2)
+  cat("\nComputing proposal distribution...\n")
+  mle <- bayesglm(f, data = d, family = binomial)
+  V <- vcov(mle)
+  #print(summary(mle))
+  mcmc.chains <- mcmc.list()
+  mcmc <- NULL
+  seed <- runif(6, 100000, 999999)  
+  run.mcmc <- function(x) {
+    init <- rnorm(ncol(X), 0, 2)
+    mcmc <- MCMCmetrop1R(fun = lp.normal, 
+                         theta.init = init,  V = V,
+                         sd = sd, which.var.sep = which.var.sep, X = X, y = y, 
+                         thin = n.thin, burnin = n.burnin, mcmc = n.sims,
+                         tune = tune, verbose = verbose,
+                         seed = list(seed, x))
+    return(mcmc)
+  }
+  cat(paste("\nRunning ", n.chains, " chains in parallel--this may take a while...", sep = ""))
+  mcmc.chains <- mclapply(1:n.chains, run.mcmc, mc.cores = 3)
+  cat(paste("\nFinished running chains!\n", sep = ""))
+  mcmc.chains <- as.mcmc.list(mcmc.chains)
+  for (i in 1:n.chains) {  
+    mcmc <- rbind(mcmc, mcmc.chains[[i]])
+  }
+  colnames(mcmc) <- colnames(X)
+  R.hat <- gelman.diag(mcmc.chains)
+  cat(paste("\nChecking convergence...\n", sep = ""))
+  if (R.hat[[2]] <= 1.02) {
+    cat(paste("\nThe multivariate R-hat statistic of ", round(R.hat[[2]], 2), 
+              " suggests that the chains have converged.\n", sep = ""))
+  }
+  if (R.hat[[2]] > 1.02) {
+    cat(paste("\n######## WARNING: #########\n\nThe multivariate R-hat statistic of ", round(R.hat[[2]], 2), 
+              " suggests that the chains have NOT converged.\n", sep = ""))
+  }
+  res <- list(mcmc.chains = mcmc.chains,
+              mcmc = mcmc,
+              R.hat = R.hat)
+  return(res)
+}
+
+# # test
+# set.seed(1234)
+# n <- 100
+# x1 <- rbinom(n, 1, .5)
+# X <- cbind(1, x1)
+# beta <- c(-1, 1)
+# y <- rbinom(n, 1, plogis(X%*%beta))
+# y[x1 == 1] <- 1
+# d <- data.frame(x1, y)
+# m1 <- normal(y ~ x1, d, sep.var = "x1", n.sims = 1000, n.burnin = 10, verbose = 0, n.chains = 4)
+# plot(m1$mcmc.chains)
 
--- bm-replication/R/mcmc.R
+++ bm-replication/R/mcmc.R
@@ -1,13 +1,8 @@
 # set working directory
-# setwd("~/Dropbox/projects/priors-for-separation")
+setwd("~/Dropbox/projects/priors-for-separation")
 
 # clear working directory
-# rm(list = ls())
-
-# load firth function
-source("br-replication/R/mcmc-firth.R")
-source("br-replication/R/mcmc-cauchy.R")
-
+rm(list = ls())
 
 # load packages
 library(compactr)
@@ -39,15 +34,95 @@ f <- warl2 ~ c.onenukedyad + c.twonukedyad + c.logCapabilityRatio +
   c.Ally + c.SmlDemocracy + c.SmlDependence + c.logDistance + 
   c.Contiguity + c.MajorPower + c.NIGOs
 
-tune <- rep(.8, 11)
-tune[3] <- 1.2
-
-# cauchy prior + mcmc
-m.cauchy.mcmc <- cauchy(f, d, mcmc = 50000, burnin = 10000, thin = 1, tune = tune, verbose = 100)
-save(m.cauchy.mcmc, file = paste("output/cauchy", Sys.time()))
-
-# firth prior + mcmc
-m.firth.mcmc <- firth(f, d, mcmc = 50000, burnin = 10000, thin = 1, tune = tune, verbose = 100)
-save(m.firth.mcmc, file = paste("output/firth", Sys.time()))
 
 
+# load firth function
+source("R/fn-mcmc-firth.R")
+source("R/fn-mcmc-cauchy.R")
+source("R/fn-mcmc-scaled-t.R")
+source("R/fn-mcmc-normal.R")
+
+
+# my prior
+m.me <- normal(f, d, sep.var = "c.twonukedyad", sd = 4.5, n.sims = 20000, n.burnin = 10000, verbose = 100, n.chains = 4)
+
+# Zorn's default
+#m.zorn <- firth(f, d, mcmc = 500, burnin = 100, thin = 1, tune = tune, verbose = 10)
+
+# Gelman's default
+#m.gelman <- cauchy(f, d, mcmc = 5000, burnin = 1000, thin = 1, tune = tune, verbose = 10)
+
+# load mcmc simulations
+load("safe/cauchy1.RData"); m.gelman <- m.cauchy.mcmc
+load("safe/firth1.RData"); m.zorn <- m.firth.mcmc
+rm(m.cauchy.mcmc, m.firth.mcmc)
+
+###########
+## plots ##
+###########
+
+my.sims <- m.me$mcmc[, "c.twonukedyad"]
+zorn.sims <- m.zorn[, 3]
+gelman.sims <- m.gelman[, 3]
+
+par(mfrow = c(1,1), mar = c(3,4,1,1), oma = c(0,0,0,0))
+
+d.me <- density(my.sims)
+d.zorn <- density(zorn.sims)
+d.gelman <- density(gelman.sims)
+
+eplot(xlim = c(-20, max(c(d.me$x, d.zorn$x, d.gelman$x))), mm(c(d.me$y, d.zorn$y, d.gelman$y)),
+      xlab = "Coefficient for Two-Nuke Dyad",
+      ylab = "Posterior Density",
+      ylabpos = 2.5)
+abline(v = 0, col = "grey50")
+lines(d.me, lwd = 2, col = 1)
+lines(d.zorn, lwd = 2, col = 2)
+lines(d.gelman, lwd = 2, col = 3)
+legend(x = par("usr")[1], y = par("usr")[4], xjust = 0, yjust = 1,
+       legend = c("My prior",
+                  "Zorn's default",
+                  "Gelman's Default"),
+       lty = 1,
+       lwd = 2,
+       col = 1:3, 
+       cex = .8,
+       bty = "n")
+
+sumry <- function(sims, ht, lab = NA) {
+  q <- quantile(sims, c(.05, .95))
+  lines(q, c(ht, ht), lwd = 2)
+  d <- density(sims, adjust = 1.5)
+  i.max <- which.max(d$y)
+  post.mode <- d$x[i.max]
+  post.mean <- mean(sims)
+  post.median <- median(sims)
+  points(post.mode, ht, pch = 19)
+  points(post.median, ht, pch = 21, bg = "white")
+  points(post.mean, ht, pch = 4)
+  text(post.mean, ht, lab, pos = 3, cex = .7)
+  print(paste("CI = ", round(q, 1)))
+  print(paste("mode = ", round(post.mode, 1)))
+  print(paste("median = ", round(post.median, 1)))
+  print(paste("mean = ", round(post.mean, 1)))
+  
+}
+
+par(mfrow = c(1,1), mar = c(3,1,1,1), oma = c(0,0,0,0))
+eplot(xlim = c(-15, 1), ylim = c(.75, 4.5),
+      anny = FALSE,
+      xlab = "Coefficient for Two-Nuke Dyad")
+abline(v = 0, lty = 2)
+sumry(my.sims, 4, "my prior")
+sumry(zorn.sims, 3, "Zorn's default")
+sumry(gelman.sims, 2, "Gelman's default")
+
+
+legend(x = par("usr")[2], y = par("usr")[4], xjust = 1, yjust = 1,
+       legend = c("mode",
+                  "median",
+                  "mean"),
+       pch = c(19, 21, 4),
+       bg = "white", 
+       cex = .8,
+       bty = "n")
